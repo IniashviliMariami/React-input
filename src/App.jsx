f7296659acad4f55b198ef42a4b28bc62de2f484
mariaminiashvili <123764110+mariaminiashvili@users.noreply.github.com>
2024-07-10 13:59:19 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css"
+import UsersList from "./components/UserList";
+
+class App extends Component{
+ 
+  render(){
+    return(
+      <UsersList/>
+    )
+  }
+}
+
+export default App
